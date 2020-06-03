7bcf6efe8c6d64f81e9677955a28e402522d4ad8
Vinicius Santos <santos.viniciusmt@gmail.com>
2020-06-03 03:04:19 -0300

    FrontEnd Tela Cadastro

---
+++ scripts/intro.js
@@ -0,0 +1,67 @@
+//comments
+// document.write('Hello');
+
+//variables
+// let myvar = 'Hello'
+// const myconst = 'He'
+
+// document.write(myconst + myvar)
+
+//String
+// const s1 = "Isso é uma string"
+// const s2 = 'Isso é uma string'
+
+// document.write(s1 + s2)
+
+//numbers
+// const n1 = 1
+// const n2 = 12
+
+// document.write(n1 + n2)
+
+//boolean - true or false
+// const bTrue = true
+// const bFalse = false
+
+// document.write(bTrue)
+
+//objeto possuem pripriedades e funcionalidades 
+// const pessoa = {
+//   altura: "1,80m",
+//   idade: 24,
+//   solteiro: true,
+//   correr(){
+//     return  "run forest"
+//   } 
+// }
+
+// document.write(pessoa.correr())
+
+//Array - vetores
+// const colecaoDeBolinhas = ["green", "blue", 2, {name: "My Name"}]
+
+// document.write(colecaoDeBolinhas[3].name)
+
+//funcoes - funcionalidades - atalhos 
+// function sayMyName(name){
+//   document.write(name)
+// }
+
+// sayMyName("Gabriel")
+// sayMyName("Vinicius")
+// sayMyName("Santos")
+
+//condiciionais
+// const notaFinal = 7;
+
+// if(notaFinal >= 5){
+//   document.write("aprovado")
+// } else{
+//   document.write("reprovado")
+// }
+
+//loop - repeticoes
+// for (i = 0; i < 10; i++){
+//   document.write("<p>hello")
+// }
+
